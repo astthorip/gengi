a593a6a4c0c4e76514bcc8d5eb146775743ede63
Astthor Ingi Petursson <astthor@dacoda.is>
2019-03-30 14:03:56 +0000

    Currency display and calculator implemented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import CurrencyList from './CurrencyList';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+<Container>
+  <Row>
+    <Col className="text-center py-5" md={{ span: 8, offset: 2 }}>
+      <h1>Gengi gjaldmiðla</h1>
+    </Col>
+  </Row>
+  <Row>
+    <Col md={{ span: 6, offset: 3 }}>
+      <CurrencyList />
+    </Col>
+  </Row>
+</Container>
     );
   }
 }
