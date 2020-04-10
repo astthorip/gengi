5eeb2182dc42b5988ee3fbd7d95ee5e68a018fcd
Astthor Ingi Petursson <astthor@dacoda.is>
2020-04-10 17:24:38 +0000

    Fixed build errors

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-transition-link`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
   ],
 }
